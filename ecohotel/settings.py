839c3d495f428207a3b7b94f48d56c53a3c8f701
andrea <andreagenovese.management@gmail.com>
2023-03-22 18:14:39 +0100

    settings.py redis

--- ecohotel/settings.py
+++ ecohotel/settings.py
@@ -30,18 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-redis_host = os.environ.get('REDIS_HOST', 'localhost')    
-# Channel layer definitions
-# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
-CHANNEL_LAYERS = {
-    "default": {
-        # This example app uses the Redis channel layer implementation asgi_redis
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(redis_host, 6379)],
-        },
-        "ROUTING": "multichat.routing.channel_routing",
-    },
+RQ_QUEUES = {
+    'default': {
+         'HOST': 'localhost',
+         'PORT': '6379',
+         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379'),  # If you're
+         'DB': 0,
+         'DEFAULT_TIMEOUT': 480,
+     }
 }
 
 # Application definition
